779a671fa8cc32826b44db3d4954b6a35f56156b
Sam Kingston <sam@sjkwi.com.au>
2010-01-27 19:56:57 +1000

    test script

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.6
+
+from gnuplot import GnuPlot
+
+g = GnuPlot('test.eps', filled_unfamiliar=True)
+
+s = """0   4.098360    3.884533
+50  55.59515    38.48895
+100 77.690662   51.967377
+200 94.654312   59.337134
+300 99.287241   64.433357
+400 99.679258   67.533856
+500 99.893086   68.104062
+800 99.893086   68.175338"""
+
+fam = []
+unfam = []
+for l in s.split('\n'):
+    vals = l.split()
+    fam.append((vals[0], vals[1]))
+    unfam.append((vals[0], vals[2]))
+
+g.plot([
+    ('familiar', fam),
+    ('unfamiliar', unfam),
+])
+
+#g.plot([
+#    ('test1', [(0, 1, 3), (1, 2, 3), (2, 3, 3)]),
+#    ('test2', [(4, 3, 3), (1, 2, 1), (0, -1, 4)]),
+#])
